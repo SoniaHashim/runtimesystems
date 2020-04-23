a4b00a8f720c1e8804e250ca9ccc8f6440b193d9
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-04-23 13:31:10 -0700

    Add ProfilerInLua profiler and update test script.

--- week1/profiler_test.lua
+++ week1/profiler_test.lua
@@ -1,21 +1,30 @@
--- Test the pepperfish profiler for a simple Lua program.
+-- Test the Pepperfish and ProfilerInLua profilers for a simple Lua program.
 
 require("pepperfish_profiler")
+require("profiler_in_lua")
 
 function loop()
-  for i=1,1000 do
-    for j = 1,1000 do
+  for i=1,10000 do
+    for j = 1,10000 do
       local k = i + j
     end
   end
   return
 end
 
+-- Pepperfish
+
 profiler = newProfiler("time")
 profiler:start()
 loop()
 profiler:stop()
-
 local outfile = io.open("profile.txt", "w+")
 profiler:report(outfile)
 outfile:close()
+
+-- ProfilerInLua
+
+Profiler:activate()
+Profiler:deactivate()
+Profiler:print_results()
+
