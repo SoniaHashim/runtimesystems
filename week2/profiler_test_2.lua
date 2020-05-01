c179adc248968d0a16ffbed5e57fb8fd4902bffc
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-05-01 11:58:32 -0700

    Add another example of slow execution with the standard interpreter.

--- week2/profiler_test_2.lua
+++ week2/profiler_test_2.lua
@@ -6,6 +6,16 @@ function add_two(x, y)
   return x + y
 end
 
+-- This checks for divisors in a stupid sort of way.
+function switch_case(x)
+	for i = x-1,2,-1 do
+		if x%i == 0 then
+			return add_two(x, i)
+		end
+	end
+	return -1
+end
+
 -- Pepperfish
 
 profiler = newProfiler("time")
@@ -14,7 +24,15 @@ for i = 1,100000000 do
   local sum = add_two(i, i)
 end
 profiler:stop()
-local outfile = io.open("profile.txt", "w+")
+local outfile = io.open("profile_results_1.txt", "w+")
+profiler:report(outfile)
+outfile:close()
+
+profiler = newProfiler("time")
+profiler:start()
+print(switch_case(99999989)) -- This is a prime number.
+profiler:stop()
+local outfile = io.open("profile_results_2.txt", "w+")
 profiler:report(outfile)
 outfile:close()
 
