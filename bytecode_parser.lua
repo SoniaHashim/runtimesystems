df57d4eb25c7563ba7ea6cc39a3349aaffebfeb7
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-05-05 19:24:13 -0700

    Check for bytecode file existence and read contents.

---
+++ bytecode_parser.lua
@@ -0,0 +1,22 @@
+-- A bytecode parser for Lua 5.3
+-- Usage: lua bytecode.luac
+
+function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then 
+  	f:close() 
+  end
+  return f ~= nil
+end
+
+function read_bytecode(file)
+	local bytecode_file = io.open(file, "rb")
+	local bytecode_content = bytecode_file:read "*a" -- *a for the entire file
+	bytecode_file:close()
+end
+
+if file_exists(arg[1]) then
+	read_bytecode(arg[1])
+else
+	print("File " .. arg[1] .. " does not exist.")
+end
