7456aa1fdd83721414996f296caa6d51f05f347f
gwynethallwright <gwyneth@ucsb.edu>
2020-05-28 22:04:13 -0700

    Add more bytecode (illustrating table creation).

---
+++ lua5.1_bytecode_parser/test_bytecode/test_code_3.lua
@@ -0,0 +1,12 @@
+-- To generate bytecode: luac5.1 -o test_code_3.luac -s $1
+
+function make_table(input_1, input_2, input_3)
+	local test_local_var = "local is cool"
+	my_new_table = {}
+	my_new_table[1] = input_1
+	my_new_table[2] = input_2
+	my_new_table[3] = input_3
+	return my_new_table, test_local_var
+end
+
+new_table, local_variable = make_table("gwyneth", "was", "here")
