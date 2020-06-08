8b1d8719aa41bde9297407393d5984ee8cf5cded
gwynethallwright <gwyneth@ucsb.edu>
2020-06-08 14:25:57 -0700

    Improvements to output (mostly indentation-related).

--- deliverables/lua5.1_bytecode_parser/bytecode_parser.lua
+++ deliverables/lua5.1_bytecode_parser/bytecode_parser.lua
@@ -25,7 +25,7 @@ opcode_types =
 opcode_descriptions =
 	{"\tCopy the value of register R(B) into register R(A).",
 	"Load the constant number Kst(Bx) into register R(A).", 
-	"Load the boolean value B into register R(A).\n\t\t\tIf C is nonzero, then skip the next instruction.",
+	"Load the boolean value B into register R(A).\n\t\t\t\tIf C is nonzero, then skip the next instruction.",
 	"Load nil values into the range of registers R(A) to R(B).",
     "Read the upvalue Up[B] into register R(A).",  
     "Read the value of the global variable Gbl[Kst(Bx)] into register R(A).", 
@@ -33,8 +33,8 @@ opcode_descriptions =
     "Write the register value R(A) into the global variable Gbl[Kst(Bx)].",
     "Write the register value R(A) into the upvalue Up[B].",
     "Write the register value R(C) into the table element R(A)[RK(B)].", 
-    "Create a new table at register R(A).\n\t\t\tB encodes the size of the array part and C the size of the hash part.",  
-    "Load a function reference from the table element R(B)[RK(C)] into register R(A).\n\t\t\tLoad the table reference R(B) into register R(A+1).",
+    "Create a new table at register R(A).\n\t\t\t\tB encodes the size of the array part and C the size of the hash part.",  
+    "Load a function reference from the table element R(B)[RK(C)] into register R(A).\n\t\t\t\tLoad the table reference R(B) into register R(A+1).",
     "Load the sum of RK(B) and RK(C) into register R(A).",  
     "Load the difference of RK(B) and RK(C) into register R(A).", 
     "Load the product of RK(B) and RK(C) into register R(A).",
@@ -42,24 +42,24 @@ opcode_descriptions =
     "Load the result of RK(B) modulo RK(C) into register R(A).",
     "Load the result of RK(B) to the power RK(C) into register R(A).", 
     "Negate the value in register R(B) and load into register R(A).",  
-    "Apply a boolean NOT to the value in register R(B).\n\t\t\tLoad the result into register R(A).",
-    "Calculate the length of the object in register R(B).\n\t\t\tLoad the result into register R(A).",  
-    "Concatenate the strings in the range of registers R(B) to R(C).\n\t\t\tLoad the result into register R(A).", 
-    "\tPerform an unconditional jump.\n\t\t\tIncrement the program counter by sBx.", 
-    "Test if the boolean result of RK(B) == RK(C) is equal to A.\n\t\t\tIf not, then skip the next instruction.",
-    "Test if the boolean result of RK(B) < RK(C) is equal to A.\n\t\t\tIf not, then skip the next instruction.",  
-    "Test if the boolean result of RK(B) <= RK(C) is equal to A.\n\t\t\tIf not, then skip the next instruction.",  
-    "Coerce register R(B) into a boolean and compare to boolean C.\n\t\t\tIf they match, then skip the next instruction.\n\t\t\tIf not, then load R(B) into R(A).", 
-    "Coerce register R(A) into a boolean and compare to boolean C.\n\t\t\tIf they match, then skip the next instruction.",
-    "Perform a function call.\n\t\t\tRegister R(A) holds the reference to the function object.\n\t\t\tThere are B-1 parameters and C-1 return values.", 
-    "Perform a tail call (single function call in return statement).\n\t\t\tRegister R(A) holds the reference to the function object.\n\t\t\tThe function takes B-1 parameters.\n\t\t\tC is always zero.",
-    "Return to the calling function.\n\t\t\tLoad B-1 return values into consecutive registers from R(A) onwards.",  
-    "Iterate a numeric for loop.\n\t\t\tFor each iteration, the program counter is incremented by sBx.",
-    "Initialize a numeric for loop.\n\t\t\tRegister R(A) holds the initial variable.\n\t\t\tR(A+1) holds the limit.\n\t\t\tR(A+2) holds the stepping value.\n\t\t\tR(A+3) holds the external index.",
-    "Iterate a generic for loop.\n\t\t\tR(A) holds the iterator function (called once per loop).\n\t\t\tR(A+1) holds the state.\n\t\t\tR(A+2) holds the enumeration index.", 
+    "Apply a boolean NOT to the value in register R(B).\n\t\t\t\tLoad the result into register R(A).",
+    "Calculate the length of the object in register R(B).\n\t\t\t\tLoad the result into register R(A).",  
+    "Concatenate the strings in the range of registers R(B) to R(C).\n\t\t\t\tLoad the result into register R(A).", 
+    "\tPerform an unconditional jump.\n\t\t\t\tIncrement the program counter by sBx.", 
+    "Test if the boolean result of RK(B) == RK(C) is equal to A.\n\t\t\t\tIf not, then skip the next instruction.",
+    "Test if the boolean result of RK(B) < RK(C) is equal to A.\n\t\t\t\tIf not, then skip the next instruction.",  
+    "Test if the boolean result of RK(B) <= RK(C) is equal to A.\n\t\t\t\tIf not, then skip the next instruction.",  
+    "Coerce register R(B) into a boolean and compare to boolean C.\n\t\t\t\tIf they match, then skip the next instruction.\n\t\t\t\tIf not, then load R(B) into R(A).", 
+    "Coerce register R(A) into a boolean and compare to boolean C.\n\t\t\t\tIf they match, then skip the next instruction.",
+    "Perform a function call.\n\t\t\t\tRegister R(A) holds the reference to the function object.\n\t\t\t\tThere are B-1 parameters and C-1 return values.", 
+    "Perform a tail call (single function call in return statement).\n\t\t\t\tRegister R(A) holds the reference to the function object.\n\t\t\t\tThe function takes B-1 parameters.\n\t\t\t\tC is always zero.",
+    "Return to the calling function.\n\t\t\t\tLoad B-1 return values into consecutive registers from R(A) onwards.",  
+    "Iterate a numeric for loop.\n\t\t\t\tFor each iteration, the program counter is incremented by sBx.",
+    "Initialize a numeric for loop.\n\t\t\t\tRegister R(A) holds the initial variable.\n\t\t\t\tR(A+1) holds the limit.\n\t\t\t\tR(A+2) holds the stepping value.\n\t\t\t\tR(A+3) holds the external index.",
+    "Iterate a generic for loop.\n\t\t\t\tR(A) holds the iterator function (called once per loop).\n\t\t\t\tR(A+1) holds the state.\n\t\t\t\tR(A+2) holds the enumeration index.", 
     "Set the values for B array elements in a table referenced by R(A) starting at index C.", 
     "Close all local variables used as upvalues in the stack from register R(A) onwards.", 
-    "Create a closure (instance) of a function prototype indexed by Bx.\n\t\t\tLoad the reference into register R(A).",
+    "Create a closure (instance) of a function prototype indexed by Bx.\n\t\t\t\tLoad the reference into register R(A).",
     "Load B-1 vararg function arguments into consecutive registers from R(A) onwards."}
 
 constant_types = {"LUA_TNIL", "LUA_TBOOLEAN", "", "LUA_TNUMBER", "LUA_TSTRING"}
@@ -170,16 +170,16 @@ end
 
 function print_initial_information()
 	print("EXPLANATION OF NOTATION USED")
-	print("R(A):\t\t\tRegister A (specified in instruction field A).")
-	print("R(B):\t\t\tRegister B (specified in instruction field B).")
-	print("R(C):\t\t\tRegister C (specified in instruction field C).")
-	print("RK(B):\t\t\tRegister B or an index into the list of constants.")
-	print("RK(C):\t\t\tRegister C or an index into the list of constants.")
-	print("sBx:\t\t\tSigned number (specified in instruction field sBx).")
-	print("Bx:\t\t\tUnsigned number (specified in instruction field Bx).")
-	print("Kst(n):\t\t\tElement n in the list of constants.")
-	print("Gbl[n]:\t\t\tGlobal variable with symbolic index n.")
-	print("Up[n]:\t\t\tName of the upvalue with index n.")
+	print("R(A):\t\t\t\tRegister A (specified in instruction field A).")
+	print("R(B):\t\t\t\tRegister B (specified in instruction field B).")
+	print("R(C):\t\t\t\tRegister C (specified in instruction field C).")
+	print("RK(B):\t\t\t\tRegister B or an index into the list of constants.")
+	print("RK(C):\t\t\t\tRegister C or an index into the list of constants.")
+	print("sBx:\t\t\t\tSigned number (specified in instruction field sBx).")
+	print("Bx:\t\t\t\tUnsigned number (specified in instruction field Bx).")
+	print("Kst(n):\t\t\t\tElement n in the list of constants.")
+	print("Gbl[n]:\t\t\t\tGlobal variable with symbolic index n.")
+	print("Up[n]:\t\t\t\tName of the upvalue with index n.")
 	print("----------------------------------------------------------------------------------------")
 end
 
@@ -200,12 +200,12 @@ function decode_header(bytes_table) -- nothing here is affected by endianness
 	else
 		number_type = "integer"
 	end
-	print("Endianness:\t\t" .. endianness_string)
-	print("int:\t\t\t" .. "The size of an integer is " .. size_int .. " bytes.")
-	print("size_t:\t\t\t" .. "The string data size parameter is " .. size_t .. " bytes.")
-	print("Instruction:\t\t" .. "The size of a bytecode instruction is " .. size_instruction .. " bytes.")
-	print("lua_Number:\t\t" .. "The size of a Lua number is " .. size_lua_number .. " bytes.")
-	print("Integral flag:\t\t" .. "The Lua number type is " .. number_type .. ".")
+	print("Endianness:\t\t\t" .. endianness_string)
+	print("int:\t\t\t\t" .. "The size of an integer is " .. size_int .. " bytes.")
+	print("size_t:\t\t\t\t" .. "The string data size parameter is " .. size_t .. " bytes.")
+	print("Instruction:\t\t\t" .. "The size of a bytecode instruction is " .. size_instruction .. " bytes.")
+	print("lua_Number:\t\t\t" .. "The size of a Lua number is " .. size_lua_number .. " bytes.")
+	print("Integral flag:\t\t\t" .. "The Lua number type is " .. number_type .. ".")
 	return endianness, integral_flag, size_int, size_t, size_instruction, size_lua_number
 end
 
@@ -219,26 +219,26 @@ function decode_function(byte_table_pointer, bytes, endianness, integral_flag, s
 	num_parameters, byte_table_pointer = get_byte(bytes, byte_table_pointer)
 	is_vararg_flag, byte_table_pointer = get_byte(bytes, byte_table_pointer)
 	if is_vararg_flag == 7 then
-		is_vararg_output = "\t\t\tFor vararg functions that do not use \"...\" in the body.\n\t\t\tCould instead be using the \"arg\" table."
+		is_vararg_output = "\t\t\t\tFor vararg functions that do not use \"...\" in the body.\n\t\t\t\tCould instead be using the \"arg\" table."
 	elseif is_vararg_flag == 3 then
-		is_vararg_output = "\t\t\tFor vararg functions that use \"...\" in the body."
+		is_vararg_output = "\t\t\t\tFor vararg functions that use \"...\" in the body."
 	elseif is_vararg_flag == 2 then
-		is_vararg_output = "\t\t\tNot a vararg function (fixed number of arguments). Main method."
+		is_vararg_output = "\t\t\t\tNot a vararg function (fixed number of arguments). Main method."
 	elseif is_vararg_flag == 0 then
-		is_vararg_output = "\t\t\tNot a vararg function (fixed number of arguments)."
+		is_vararg_output = "\t\t\t\tNot a vararg function (fixed number of arguments)."
 	end
 	max_stack_size, byte_table_pointer = get_byte(bytes, byte_table_pointer)
 	num_instructions, byte_table_pointer = get_int(bytes, byte_table_pointer, size_int, endianness)
-	print("Line defined:\t\t" .. start_line)
-	print("Last line defined:\t" .. end_line)
-	print("Number of upvalues:\t" .. num_upvalues)
-	print("Number of parameters:\t" .. num_parameters)
-	print("is_vararg_flag:\t\t" .. is_vararg_flag)
+	print("Line defined:\t\t\t" .. start_line)
+	print("Last line defined:\t\t" .. end_line)
+	print("Number of upvalues:\t\t" .. num_upvalues)
+	print("Number of parameters:\t\t" .. num_parameters)
+	print("is_vararg_flag:\t\t\t" .. is_vararg_flag)
 	if is_vararg_output then
 		print(is_vararg_output)
 	end
-	print("Maximum stack size:\t" .. max_stack_size)
-	print("Number of instructions:\t" .. num_instructions)
+	print("Maximum stack size:\t\t" .. max_stack_size)
+	print("Number of instructions:\t\t" .. num_instructions)
 	print("--------------------------------------------------")
 	if num_instructions > 0 then
 		for i = 1, num_instructions do
@@ -278,12 +278,12 @@ function decode_function(byte_table_pointer, bytes, endianness, integral_flag, s
 				instruction_operands = A_register_index
 			end
 			-- +1 for table lookup because instruction numbers start at zero
-			print(opcode_names[decimal_opcode + 1] .. " " .. instruction_operands .. " (" .. instruction_type .. "): \t" .. opcode_descriptions[decimal_opcode + 1]) 
+			print(opcode_names[decimal_opcode + 1] .. " " .. instruction_operands .. " (" .. instruction_type .. "):\t\t" .. opcode_descriptions[decimal_opcode + 1]) 
 		end
 		print("--------------------------------------------------")
 		-- moving on to the constants
 		num_constants, byte_table_pointer = get_int(bytes, byte_table_pointer, size_int, endianness)
-		print("Number of constants:\t" .. num_constants)
+		print("Number of constants:\t\t" .. num_constants)
 		for i = 1, num_constants do
 			constant_type_index, byte_table_pointer = get_byte(bytes, byte_table_pointer)
 			-- +1 for table lookup because constant numbers start at zero
@@ -305,12 +305,12 @@ function decode_function(byte_table_pointer, bytes, endianness, integral_flag, s
 			else
 				constant_data = "" -- LUA_TNIL has nothing
 			end
-			print("Type " .. constant_type .. ":\t" .. constant_data)
+			print("Type " .. constant_type .. ":\t\t" .. constant_data)
 		end
 		print("--------------------------------------------------")
 		-- moving on to the function prototypes
 		num_prototypes, byte_table_pointer = get_int(bytes, byte_table_pointer, size_int, endianness)
-		print("Number of function prototypes: " .. num_prototypes)
+		print("Number of function prototypes:\t" .. num_prototypes)
 		if num_prototypes > 0 then
 			for i = 1, num_prototypes do
 				byte_table_pointer = decode_function(byte_table_pointer, bytes, endianness, integral_flag, size_int, size_t, size_instruction, size_lua_number)
@@ -363,8 +363,8 @@ function read_bytecode(file)
 		-- \xNN escape character: NN is a two-digit hex number
 		print("The file " .. file .. " is not a Lua bytecode file.")
 	elseif bytecode_content:sub(5, 5) ~= "\x51" then
-		print("This bytecode parser requires version 5.1 of the lua bytecode.")
-		print("You are currently using version " .. string.format("%02X", bytecode_content:sub(5, 5):byte(1)):sub(1, 1) 
+		print("This bytecode parser requires version 5.1 of the Lua bytecode.")
+		print("Your bytecode file was compiled using version " .. string.format("%02X", bytecode_content:sub(5, 5):byte(1)):sub(1, 1) 
 			.. "." .. string.format("%02X", bytecode_content:sub(5, 5):byte(1)):sub(2, 2) .. ". Aborting.")
 	else
 		print_initial_information()
